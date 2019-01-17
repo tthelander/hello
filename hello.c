2c78795a8673d0595fadcaff3d91998ab343a879
Trevor Thelander <thelander.trevor@yahoo.com>
2019-01-17 14:53:20 -0600

    Create hello.c
    Added a line to print my major

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Trevor Thelander and Drew Borchers
+ * Date: 2019/1/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Trevor Thelander and Drew Borchers\n");
+  printf("Computer Engineering\n");
+  return 0;
+}
